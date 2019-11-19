b97a3eb7ae3b631452d638dff35d34808e3b1655
lucas.blin <lucas.blin@visiativ.com>
2019-11-19 19:40:19 +0100

    Add Auth0 authentification

--- User/src/main.go
+++ User/src/main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
+	auth0 "../github/auth0"
 	user "../github/user"
+
 	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
 )
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
+	jwtMiddleware := auth0.Initiate()
+
+	//Example route to use if want to authenticate the user
+	router.Handle("/users/auth0", negroni.New(
+		negroni.HandlerFunc(jwtMiddleware.HandlerWithNext),
+		negroni.Wrap(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			response := auth0.Response{}
+			response.Message = "OK"
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(response)
+		})))).Methods("GET")
+
 	//Add an user to database
 	router.HandleFunc("/users", user.Create).Methods("POST")
 
