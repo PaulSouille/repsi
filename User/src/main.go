2ebb84c96f97f9e256ebb932f1cf56588867d79c
lucas.blin <lucas.blin@visiativ.com>
2019-11-21 07:50:23 +0100

    add auth0 to all routes

--- User/src/main.go
+++ User/src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 
@@ -16,24 +15,20 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	jwtMiddleware := auth0.Initiate()
 
-	//Example route to use if want to authenticate the user
-	router.Handle("/users/auth0", negroni.New(
-		negroni.HandlerFunc(jwtMiddleware.HandlerWithNext),
-		negroni.Wrap(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			response := auth0.Response{}
-			response.Message = "OK"
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(response)
-		})))).Methods("GET")
-
 	//Add an user to database
-	router.HandleFunc("/users", user.Create).Methods("POST")
+	router.Handle("/users", negroni.New(
+		negroni.HandlerFunc(jwtMiddleware.HandlerWithNext),
+		negroni.Wrap(http.HandlerFunc(user.Create)))).Methods("POST")
 
 	//Get an user by his mail address
-	router.HandleFunc("/users/{user-mail}", user.Search).Methods("GET")
+	router.Handle("/users/{user-mail}", negroni.New(
+		negroni.HandlerFunc(jwtMiddleware.HandlerWithNext),
+		negroni.Wrap(http.HandlerFunc(user.Search)))).Methods("GET")
 
 	//Delete an user with his id/mail
-	router.HandleFunc("/users", user.Delete).Methods("DELETE")
+	router.Handle("/users", negroni.New(
+		negroni.HandlerFunc(jwtMiddleware.HandlerWithNext),
+		negroni.Wrap(http.HandlerFunc(user.Delete)))).Methods("DELETE")
 
 	//Serve HTTP server
 	log.Fatal(http.ListenAndServe(":8080", router))
