5985be118a6011faea4472ca4b204b3662833658
lucas.blin <lucas.blin@visiativ.com>
2019-11-21 18:52:05 +0100

    add json response status

--- User/src/main.go
+++ User/src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -12,15 +13,14 @@ import (
 )
 
 func main() {
+
 	router := mux.NewRouter().StrictSlash(true)
 	jwtMiddleware := auth0.Initiate()
 
-	//Add an user to database
 	router.Handle("/users", negroni.New(
 		negroni.HandlerFunc(jwtMiddleware.HandlerWithNext),
 		negroni.Wrap(http.HandlerFunc(user.Create)))).Methods("POST")
 
-	//Get an user by his mail address
 	router.Handle("/users/{user-mail}", negroni.New(
 		negroni.HandlerFunc(jwtMiddleware.HandlerWithNext),
 		negroni.Wrap(http.HandlerFunc(user.Search)))).Methods("GET")
@@ -36,5 +36,6 @@ func main() {
 		negroni.Wrap(http.HandlerFunc(user.Update)))).Methods("PUT")
 
 	//Serve HTTP server
+	fmt.Println("Server listening on port :8080 - Can now listen to your queries")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
